5853581d6d2d11445553c9328199497d0b5ebe95
Binary-Luke <lukesousa28@gmail.com>
2022-12-14 22:40:35 -0600

    fixed api routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -8,13 +8,13 @@ api = Blueprint('api', __name__, url_prefix='/api')
 def getData():
     return {'yee': 'haw'}
 
-@api.route('/contacts', methods = ['POST'])
+@api.route('/books', methods = ['POST'])
 @token_required
-def create_contact(current_user_token):
-    name = request.json['name']
-    email = request.json['email']
-    phone_number = request.json['phone_number']
-    address = request.json['address']
+def create_book(current_user_token):
+    title = request.json['title']
+    author = request.json['author']
+    ISBN = request.json['ISBN']
+    edition = request.json['edition']
     user_token = current_user_token.token
 
     print(f'BIG TESTER: {current_user_token.token}')
